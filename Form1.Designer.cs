44ee77b9f7fe358e77c41d3d7213a3503c993cce
erdal <erdal@LAPTOP-PD0QNU5F>
2023-05-26 14:24:24 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Ödev_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATEMATİK UYGULAMASI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(107, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayi Giriniz: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(100, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 42);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Faktöriyel: ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(100, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 42);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Asal Sayi mi:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(100, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 42);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kendisine kadar olan sayılar:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(244, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(27, 1);
+            this.panel1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(244, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 27);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(277, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "120";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(277, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Asal";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(277, 321);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "1-2-3-4-5";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(161, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(406, 348);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Ödev_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      void temizle()//hatalar için kullancağım bazı alanlardaki verileri temizlemek için
+        {
+            textBox1.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+        }
+       public static int faktoriyel(int sayi)//Faktoriyeli recursivi olarak bulan paremetre alan geriye değer dödürene metod
+        {
+            if (sayi <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return sayi*faktoriyel(sayi - 1);
+            }
+        }
+        void asal(int sayi)//asal sayıyı bulacak geriye değer döndürmeyen paremetreli metod
+        {
+           
+            int sayac = 0;
+            for(int i = 2; i <= sayi/2; i++)//2'ye bölme sebebim bölmenin tanımından geliyor
+            {
+                if (sayi % i==0) {
+                sayac++;
+                    break;//bir tane bile varsa direkt çıkacak böylece zaman kaybetmeyecek
+                }
+            }
+            if(sayac!=0||sayi == 1 || sayi == 0)// 1 ile 0'ı ayrıyetten belirtmek zorunda kaldım
+            {
+                label7.Text ="Asal Değil";
+            }
+            else
+            {
+                label7.Text = "Asal";
+            }
+            
+
+        }
+        void sıralama(int sayi)// sıralamak için  geriye değer döndürmeyen paremetreli metod
+        {
+            label8.Text = "";
+            for(int i = 1; i < sayi; i++)
+            {
+                label8.Text +=  i+"-" ;//ekleyerek label'i yazacak en sonda da '-' olacak onun çözümünü iki satır aşağıda
+            }
+            label8.Text += sayi;//sayının en sonunda '-' işareti geliyordu o sorunu bu şekil çözebildim substring denedim ama olmadı
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int girdi;
+            try
+            {
+
+                girdi = int.Parse(textBox1.Text);//kullanıcadan değer aldık
+               //Bulmak istediğimiz mat işlemleri için kullanıcan aldığımız değerleri metodlara gönderiyoruz
+                if (girdi >= 0)// asal sayı ve faktoriyel negatif sayılarda olmadığı için negatif sayılarda işlem yapamayız
+                {
+                    label6.Text = faktoriyel(girdi).ToString();
+                    asal(girdi);
+                    sıralama(girdi);
+                }
+                else
+                {
+                    MessageBox.Show("Girilen sayı negatif olamaz");
+                    temizle();
+                }
+            }
+            catch (FormatException)//Eğer sayı yerine karekter girerse bu hatayı verecek bu yüzden bir sıkıntı yaşamayacağız
+            {
+                MessageBox.Show("Lütfen Alana Sayısal Değer giriniz");
+                temizle();
+            }
+
+            textBox1.Focus();//Devamlı maouse kullanmamak için direkt textbox!a odaklayacak
+        }
+    }
+}
